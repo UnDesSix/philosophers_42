d84bcb59932cb39228ddbb48e1d089fb92cb1a66
UnDesSix <matthieu.larboullet@gmail.com>
2021-06-04 16:46:34 +0200

    Need to be tested - and change mutex variable

--- test.c
+++ test.c
@@ -2,30 +2,28 @@
 #include <stdio.h>
 #include <pthread.h>
 
-void	*routine1(void *tom)
+void	*routine(int	*i, MUTEX)
 {
-	int i = 0;
-//	while (i < 10000000)
-//		i++;
-	printf("Hello routine 1\n");
+	while (*i < 10000000)
+	{
+		pthread_mutex_lock(MUTEX);
+		(*i)++;
+		pthread_mutex_unlock(MUTEX);
+	}
 	return (NULL);
 }
-
-void	*routine2(void *matt )
-{
-	printf("Hello routine 2\n");
-	return (NULL);
-}
-
 
 int		main(void)
 {
 	pthread_t	t1;
 	pthread_t	t2;
-
-	pthread_create(&t1, NULL, &routine1, NULL);
-	pthread_create(&t2, NULL, &routine2, NULL);
+	int 		i;
+	
+	i = 0;
+	pthread_create(&t1, NULL, &routine(&i), NULL);
+	pthread_create(&t2, NULL, &routine(&i), NULL);
 	pthread_join(t1, NULL);
 	pthread_join(t2, NULL);
+	printf("i : %d\n", i);
 	return (0);
 }
