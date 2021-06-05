7ed0effcc7de9a368dfa07511b4d222e06b993ef
mlarboul <mlarboul@student.42.fr>
2021-06-05 07:21:35 +0200

    let s try this code on mac

--- test.c
+++ test.c
@@ -2,28 +2,41 @@
 #include <stdio.h>
 #include <pthread.h>
 
-void	*routine(int	*i, MUTEX)
+//	pthread_mutex_t	mutex;
+int		i = 0;
+
+void	*routine1()
 {
-	while (*i < 10000000)
-	{
-		pthread_mutex_lock(MUTEX);
-		(*i)++;
-		pthread_mutex_unlock(MUTEX);
-	}
+	while (i < 1)
+		i++;
 	return (NULL);
 }
 
-int		main(void)
+void	*routine2()
 {
-	pthread_t	t1;
-	pthread_t	t2;
-	int 		i;
-	
-	i = 0;
-	pthread_create(&t1, NULL, &routine(&i), NULL);
-	pthread_create(&t2, NULL, &routine(&i), NULL);
-	pthread_join(t1, NULL);
-	pthread_join(t2, NULL);
+	while (i < 1)
+		i++;
+	return (NULL);
+}
+
+int		multi_thread(void)
+{
+	pthread_t		p1, p2;
+
+	if (pthread_create(&p1, NULL, &routine1, NULL) != 0)
+		return (-1);
+	if (pthread_create(&p2, NULL, &routine2, NULL) != 0)
+		return (-2);
+	if (pthread_join(p1, NULL) != 0)
+		return (-3);
+	if (pthread_join(p2, NULL) != 0)
+		return (-4);
 	printf("i : %d\n", i);
+	return (0);
+}
+
+int main(void)
+{
+	printf("\nReturn Value = %d\n", multi_thread());
 	return (0);
 }
