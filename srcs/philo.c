ad85b1cce8b0b1c05d9cb7e8e1f29536b18b1318
mlarboul <mlarboul@student.42.fr>
2021-06-09 10:50:29 +0200

    debug options + options

--- srcs/philo.c
+++ srcs/philo.c
@@ -6,16 +6,29 @@
 /*   By: mlarboul <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/09 07:09:40 by mlarboul          #+#    #+#             */
-/*   Updated: 2021/06/09 08:19:17 by mlarboul         ###   ########.fr       */
+/*   Updated: 2021/06/09 10:49:06 by mlarboul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
 
+void	check_opt_values(t_opt *options)
+{
+	printf("philo_nb : %d\n", options->philo_nb);
+	printf("time to eat : %d\n", options->time_to_die);
+	printf("time to die: %d\n", options->time_to_eat);
+	printf("time to sleep : %d\n", options->time_to_sleep);
+	printf("extra nb : %d\n", options->extra_nb);
+}
+
 int	main(int argc, char **argv)
 {
+	t_opt	*options;
+
 	if (valid_arg(argc, argv) == FALSE)
 		return (0);
-	printf("ALL IS OK\n");
+	options = save_options(argc, argv);
+	check_opt_values(options);
+	free(options);
 	return (0);
 }
