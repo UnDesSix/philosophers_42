9febd989504c0d57964ac8b636a66d9a1ecba205
mlarboul <mlarboul@student.42.fr>
2021-06-09 08:35:20 +0200

    check args OK

---
+++ srcs/check_args.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_args.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlarboul <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/09 07:23:43 by mlarboul          #+#    #+#             */
+/*   Updated: 2021/06/09 08:27:26 by mlarboul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/philo.h"
+
+t_bool	valid_arg(int argc, char **argv)
+{
+	int	i;
+	int	valid;
+
+	i = 0;
+	valid = TRUE;
+	if (argc < 5 || argc > 6)
+	{
+		printf("Error - Wrong numbers of arguments.\n");
+		return (FALSE);
+	}
+	while (++i < argc)
+		if (arg_is_wrong(i, ft_atoi(argv[i])) == TRUE)
+			valid = FALSE;
+	if (valid == FALSE)
+		return (FALSE);
+	return (TRUE);
+}
