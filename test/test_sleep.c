a01f1141cd48e0b2da3bc699d435f7642944f838
mlarboul <mlarboul@student.42.fr>
2021-06-09 08:32:44 +0200

    some programm to learn about thread and functions about tome

---
+++ test/test_sleep.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <pthread.h>
+
+/*
+int		main()
+{
+	int i;
+
+	i = 0;
+	while (i++ < 20)
+		sleep(1);
+	return (0);
+}*/
