4a3a03e30c7e127e151237db9e63c7e1f01d2461
mlarboul <mlarboul@student.42.fr>
2021-06-17 14:59:16 +0200

    adding new features

--- srcs/create_philo.c
+++ srcs/create_philo.c
@@ -6,33 +6,25 @@
 /*   By: mlarboul <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/09 10:57:56 by mlarboul          #+#    #+#             */
-/*   Updated: 2021/06/16 19:15:42 by mlarboul         ###   ########.fr       */
+/*   Updated: 2021/06/17 14:28:00 by mlarboul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
 
-void	*routine(void *arg)
+void	free_all(t_arg *arg)
 {
-	t_arg	*arg_bis;
-	int		fork[2];
-	t_philo	*philos;
-	int		i;
+	int	i;
 
-	arg_bis = (t_arg *)arg;
-	i = arg_bis->i;
-	philos = arg_bis->philos;
-	printf("I'm philosopher %.2d, ", philos[i].id);
-	if (i + 1 - 1 < 1)
-		fork[0] = philos[i].options->philo_nb;
-	else
-		fork[0] = i + 1 - 1;
-	if (i + 1 + 1 > philos[i].options->philo_nb)
-		fork[1] = 1;
-	else
-		fork[1] = i + 1;
-	printf("and I can use fork %d and %d to eat.\n", fork[0], fork[1]);
-	return (NULL);
+	i = -1;
+	while (++i < arg->philos->options->philo_nb)
+		pthread_mutex_destroy(&arg->mutex[i]);
+	if (arg->philos != NULL)
+		free(arg->philos);
+	if (arg->mutex != NULL)
+		free(arg->mutex);
+	if (arg != NULL)
+		free(arg);
 }
 
 t_philo	*set_table(t_opt *options)
@@ -48,39 +40,65 @@ t_philo	*set_table(t_opt *options)
 	{
 		philos[i].options = options;
 		philos[i].id = i + 1;
-		philos[i].fork = i + 1;
+		if (i < 1)
+			philos[i].l_fork = options->philo_nb;
+		else
+			philos[i].l_fork = i;
+		philos[i].r_fork = i + 1;
+//		printf("Philos %d must eat with fork %d and %d.\n",
+//				philos[i].id, philos[i].l_fork, philos[i].r_fork);
 		i++;
 	}
 	return (philos);
 }
 
-int	create_philo(t_opt *options)
+t_arg	*init_arg(t_opt *options)
 {
-	int		i;
-	t_arg	*arg;
-	t_philo	*tmp;
+	t_arg			*arg;
+	t_philo			*philos;
+	pthread_mutex_t	*mutex;
+	struct timeval	start;
+	int				i;
 
-	i = -1;
 	arg = malloc(sizeof(t_arg) * options->philo_nb);
 	if (arg == NULL)
-		return (-1);
-	tmp = set_table(options);
-	if (tmp == NULL)
-		return (-1);
-	while (++i < options->philo_nb)
-		arg[i].philos = tmp;
+		return (NULL);
+	arg->all_alive = TRUE;
+	philos = set_table(options);
+	if (philos == NULL)
+		return (NULL);
+	mutex = malloc(sizeof(pthread_mutex_t) * options->philo_nb);
+	if (mutex == NULL)
+		return (NULL);
 	i = -1;
+	gettimeofday(&start, NULL);
 	while (++i < options->philo_nb)
 	{
+		arg[i].philos = philos;
 		arg[i].i = i;
-		pthread_create(&arg[i].philos[i].th, NULL, &routine, &arg[i]);
+		arg[i].mutex = mutex;
+		arg[i].start = start;
+		pthread_mutex_init(&mutex[i], NULL);
 	}
+	return (arg);
+}
+
+int	create_philo(t_opt *options)
+{
+	t_arg	*arg;
+	int		i;
+
+	arg = init_arg(options);
+	if (arg == NULL)
+		return (-1);
 	i = -1;
 	while (++i < options->philo_nb)
-		pthread_join(arg[i].philos[i].th, NULL);
-	if (tmp != NULL)
-		free(tmp);
-	if (arg != NULL)
-		free(arg);
+		if (pthread_create(&arg[i].philos[i].th, NULL, &routine, &arg[i]) != 0)
+			return (-1);
+	i = -1;
+	while (++i < options->philo_nb)
+		if (pthread_join(arg[i].philos[i].th, NULL) != 0)
+			return (-1);
+	free_all(arg);
 	return (0);
 }
