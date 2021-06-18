84b5a7fcddcbc4e0874cbefb4d7729c703e88d6c
mlarboul <mlarboul@student.42.fr>
2021-06-18 10:16:33 +0200

    Even grab right - Odd grab left

--- srcs/routine.c
+++ srcs/routine.c
@@ -6,7 +6,7 @@
 /*   By: mlarboul <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/17 08:05:33 by mlarboul          #+#    #+#             */
-/*   Updated: 2021/06/17 14:44:05 by mlarboul         ###   ########.fr       */
+/*   Updated: 2021/06/18 10:11:41 by mlarboul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,42 @@ long	get_timestamp(struct timeval begin)
 	return ((end - start) / 1E3);
 }
 
+int	grab_fork(t_arg *table, t_philo *philos, int i)
+{
+//	int	left_fork_res;
+//	int	right_fork_res;
+
+/*	if (i % 2 == 0)
+		right_fork_res = pthread_mutex_lock(&table->mutex[philos[i].r_fork - 1]);
+	left_fork_res = pthread_mutex_lock(&table->mutex[philos[i].l_fork - 1]);
+	if (i % 2 == 1)
+		right_fork_res = pthread_mutex_lock(&table->mutex[philos[i].r_fork - 1]);
+	if (left_fork_res == 0 && right_fork_res == 0)
+		return (0);
+	else if (left_fork_res == 0 && right_fork_res != 0)
+	{
+		pthread_mutex_unlock(&table->mutex[philos[i].l_fork - 1]);
+		grab_fork(table, philos, i);
+	}
+	else if (left_fork_res != 0 && right_fork_res == 0)
+	{
+		pthread_mutex_unlock(&table->mutex[philos[i].r_fork - 1]);
+		grab_fork(table, philos, i);
+	}
+	else
+		grab_fork(table, philos, i);
+*/
+	if (i % 2 == 0)
+		while (pthread_mutex_lock(&table->mutex[philos[i].r_fork - 1]))
+			;
+	while (pthread_mutex_lock(&table->mutex[philos[i].l_fork - 1]))
+		;
+	if (i % 2 == 1)
+		while (pthread_mutex_lock(&table->mutex[philos[i].r_fork - 1]))
+			;
+	return (0);
+}
+
 void	*routine(void *arg)
 {
 	t_arg		*table;
@@ -47,24 +83,17 @@ void	*routine(void *arg)
 	table = (t_arg *)arg;
 	i = table->i;
 	philos = table->philos;
-	while (table->all_alive)
+	while (1)
 	{
-		printf("I'm stuck here, BITCH 0\n");
-		while (pthread_mutex_lock(&table->mutex[philos[i].l_fork - 1]) != 0)
-			printf("I'm stuck here, BITCH 1\n");
-		printf("I'm stuck here, BITCH 10\n");
-		while (pthread_mutex_lock(&table->mutex[philos[i].r_fork - 1]) != 0)
-			printf("I'm stuck here, BITCH 2\n");
+		grab_fork(table, philos, i);
 		printf("%.5ld %d has taken a left fork\n", get_timestamp(table->start), philos[i].id);
 		printf("%.5ld %d has taken a right fork\n", get_timestamp(table->start), philos[i].id);
 		printf("%.5ld %d is eating\n", get_timestamp(table->start), philos[i].id);
-//		my_usleep(philos[i].options->time_to_eat);
-		my_usleep(44);
+		my_usleep(philos[i].options->time_to_eat * 1000);
 		pthread_mutex_unlock(&table->mutex[philos[i].l_fork - 1]);
 		pthread_mutex_unlock(&table->mutex[philos[i].r_fork - 1]);
 		printf("%.5ld %d is sleeping\n", get_timestamp(table->start), philos[i].id);
-		my_usleep(255);
-//		my_usleep(philos[i].options->time_to_sleep);
+		my_usleep(philos[i].options->time_to_sleep * 1000);
 		printf("%.5ld %d is thinking\n", get_timestamp(table->start), philos[i].id);
 	}
 	return (NULL);
