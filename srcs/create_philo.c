a9c67a3f61f363761695c6b8c5e04dde77553d71
mlarboul <mlarboul@student.42.fr>
2021-06-20 10:43:12 +0200

    OK - check mutex

--- srcs/create_philo.c
+++ srcs/create_philo.c
@@ -6,7 +6,7 @@
 /*   By: mlarboul <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/09 10:57:56 by mlarboul          #+#    #+#             */
-/*   Updated: 2021/06/19 11:55:10 by mlarboul         ###   ########.fr       */
+/*   Updated: 2021/06/20 10:37:32 by mlarboul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,8 @@ void	free_all(t_arg *arg)
 		free(arg->philos);
 	if (arg->mutex != NULL)
 		free(arg->mutex);
+	if (arg->all_alive)
+		free(arg->all_alive);
 	if (arg != NULL)
 		free(arg);
 }
@@ -59,7 +61,7 @@ t_arg	*init_arg(t_opt *options)
 	pthread_mutex_t	*mutex;
 	struct timeval	start;
 	int				i;
-	t_bool			all_alive;
+	t_bool			*all_alive;
 
 	arg = malloc(sizeof(t_arg) * options->philo_nb);
 	if (arg == NULL)
@@ -70,7 +72,10 @@ t_arg	*init_arg(t_opt *options)
 	mutex = malloc(sizeof(pthread_mutex_t) * options->philo_nb);
 	if (mutex == NULL)
 		return (NULL);
-	all_alive = TRUE;
+	all_alive = malloc(sizeof(t_bool));
+	if (all_alive == NULL)
+		return (NULL);
+	*all_alive = TRUE;
 	i = -1;
 	gettimeofday(&start, NULL);
 	while (++i < options->philo_nb)
@@ -79,34 +84,44 @@ t_arg	*init_arg(t_opt *options)
 		arg[i].i = i;
 		arg[i].mutex = mutex;
 		arg[i].start = start;
-		arg[i].all_alive = &all_alive;
+		arg[i].all_alive = all_alive;
 		pthread_mutex_init(&mutex[i], NULL);
 	}
 	return (arg);
+}
+
+t_bool	everyone_ate(t_arg *arg, t_opt *options)
+{
+	int	i;
+
+	i = 0;
+	while (i < options->philo_nb)
+	{
+		if (options->extra_nb > arg->philos[i].meals_nb)
+			return (FALSE);
+		i++;
+	}
+	return (TRUE);
 }
 
 t_bool	end_conditions(t_arg *arg, t_opt *options)
 {
 	int	i;
 
-	usleep(5000);
 	i = 0;
+	if (options->extra_nb >= 0 && everyone_ate(arg, options) == TRUE)
+	{
+		*arg->all_alive = FALSE;
+		return (TRUE);
+	}
 	while (i < options->philo_nb)
 	{
-		if (get_timestamp(arg->philos[i].last_meal)
-				> options->time_to_die * 1E3)
-		{
-			*arg->all_alive = FALSE;
-			printf("%.5ld %d DIEED\n\n\n\n",
-					(long)(get_timestamp(arg->start) / 1E3), arg->philos[i].id);
-			return (TRUE);
-		}
-		if (options->extra_nb >= 0 &&
-				options->extra_nb <= arg->philos[i].meals_nb)
+		if (get_timestamp(arg->philos[i].last_meal) >
+				options->time_to_die)
 		{
 			*arg->all_alive = FALSE;
 			printf("%.5ld %d died\n",
-					(long)(get_timestamp(arg->start) / 1E3), arg->philos[i].id);
+					get_timestamp(arg->start), arg->philos[i].id);
 			return (TRUE);
 		}
 		i++;
@@ -118,19 +133,32 @@ int	create_philo(t_opt *options)
 {
 	t_arg	*arg;
 	int		i;
+	int		already_died;
+
 
 	arg = init_arg(options);
 	if (arg == NULL)
 		return (-1);
 	i = -1;
+	already_died = FALSE;
 	while (++i < options->philo_nb)
+	{
 		if (pthread_create(&arg[i].philos[i].th, NULL, &routine, &arg[i]) != 0)
 			return (-1);
-	while (end_conditions(arg, options) == FALSE)
-		;
-//	i = -1;
-//	while (++i < options->philo_nb)
-//		if (pthread_join(arg[i].philos[i].th, NULL) != 0)
+		if (*arg->all_alive == TRUE && end_conditions(arg, options) == TRUE)
+		{
+			options->philo_nb = i;
+			already_died = TRUE;
+			break ;
+		}
+	}
+	while (already_died == FALSE && end_conditions(arg, options) == FALSE)
+	{
+		usleep(100);
+	}
+	i = -1;
+	while (++i < options->philo_nb)
+		pthread_detach(arg[i].philos[i].th);
 //			return (-1);
 	free_all(arg);
 	return (0);
--- srcs/routine.c
+++ srcs/routine.c
@@ -6,7 +6,7 @@
 /*   By: mlarboul <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/17 08:05:33 by mlarboul          #+#    #+#             */
-/*   Updated: 2021/06/19 11:56:44 by mlarboul         ###   ########.fr       */
+/*   Updated: 2021/06/20 10:34:15 by mlarboul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,21 @@
 
 void	start_eating(t_arg *table, t_philo *philos, int i)
 {
-	printf("%.5ld %d is eating\n",
-			(long)(get_timestamp(table->start) / 1E3), philos[i].id);
-	my_usleep(philos[i].options->time_to_eat);
+	if (*table->all_alive == TRUE)
+		printf("%.5ld %d is eating\n",
+				get_timestamp(table->start), philos[i].id);
 	gettimeofday(&philos[i].last_meal, NULL);
+	my_usleep(philos[i].options->time_to_eat);
 	philos[i].meals_nb++;
 	pthread_mutex_unlock(&table->mutex[philos[i].l_fork - 1]);
 	pthread_mutex_unlock(&table->mutex[philos[i].r_fork - 1]);
-	printf("%.5ld %d is sleeping\n",
-			(long)(get_timestamp(table->start) / 1E3), philos[i].id);
+	if (*table->all_alive == TRUE)
+		printf("%.5ld %d is sleeping\n",
+				get_timestamp(table->start), philos[i].id);
 	my_usleep(philos[i].options->time_to_sleep);
-	printf("%.5ld %d is thinking\n",
-			(long)(get_timestamp(table->start) / 1E3), philos[i].id);
+	if (*table->all_alive == TRUE)
+		printf("%.5ld %d is thinking\n",
+				get_timestamp(table->start), philos[i].id);
 }
 
 int	grab_fork(t_arg *table, t_philo *philos, int i)
@@ -34,19 +37,22 @@ int	grab_fork(t_arg *table, t_philo *philos, int i)
 	{
 		if (pthread_mutex_lock(&table->mutex[philos[i].r_fork - 1]) != 0)
 			return (-1);
-		printf("%.5ld %d has taken a fork\n",
-				(long)(get_timestamp(table->start) / 1E3), philos[i].id);
+		if (*table->all_alive == TRUE)
+			printf("%.5ld %d has taken a fork\n",
+					get_timestamp(table->start), philos[i].id);
 	}
 	if (pthread_mutex_lock(&table->mutex[philos[i].l_fork - 1]) != 0)
 		return (-1);
-	printf("%.5ld %d has taken a fork\n", 
-			(long)(get_timestamp(table->start) / 1E3), philos[i].id);
+	if (*table->all_alive == TRUE)
+		printf("%.5ld %d has taken a fork\n", 
+				get_timestamp(table->start), philos[i].id);
 	if (i % 2 == 1)
 	{
 		if (pthread_mutex_lock(&table->mutex[philos[i].r_fork - 1]) != 0)
 			return (-1);
-		printf("%.5ld %d has taken a fork\n",
-				(long)(get_timestamp(table->start) / 1E3), philos[i].id);
+		if (*table->all_alive == TRUE)
+			printf("%.5ld %d has taken a fork\n",
+					get_timestamp(table->start), philos[i].id);
 	}
 	return (0);
 }
@@ -56,14 +62,19 @@ void	*routine(void *arg)
 	t_arg		*table;
 	t_philo		*philos;
 	int			i;
+	int			j;
 
 	table = (t_arg *)arg;
 	i = table->i;
 	philos = table->philos;
+	j = 0;
 	while (*table->all_alive == TRUE)
 	{
+		if (philos->options->extra_nb > 0 && j >= philos->options->extra_nb)
+			break;
 		if (grab_fork(table, philos, i) == 0)
 			start_eating(table, philos, i);
+		j++;
 	}
 	return (NULL);
 }
