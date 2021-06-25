ef9b0139776d1f856632fc76e1b72325e8b6d228
mlarboul <mlarboul@student.42.fr>
2021-06-25 17:05:00 +0200

    timer precision OK

--- srcs/time.c
+++ srcs/time.c
@@ -6,7 +6,7 @@
 /*   By: mlarboul <mlarboul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/23 11:23:44 by mlarboul          #+#    #+#             */
-/*   Updated: 2021/06/24 19:34:27 by mlarboul         ###   ########.fr       */
+/*   Updated: 2021/06/25 17:04:10 by mlarboul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,9 @@
 long int	get_time(void)
 {
 	struct timeval	tmp;
-	long int		microseconds;
 
 	gettimeofday(&tmp, NULL);
-	microseconds = tmp.tv_sec * 1E6;
-	microseconds += tmp.tv_usec;
-	return (microseconds);
+	return (tmp.tv_sec * 1000 + tmp.tv_usec / 1000);
 }
 
 int	my_usleep(long int ms, t_arg *table)
@@ -29,7 +26,7 @@ int	my_usleep(long int ms, t_arg *table)
 	long int	end;
 
 	curr = get_time();
-	end = curr + ms * 1E3;
+	end = curr + ms;
 	while (get_time() < end && is_alive(table) == TRUE)
 		usleep(100);
 	return (0);
@@ -41,8 +38,8 @@ long	get_timestamp(struct timeval begin)
 	long			end;
 	struct timeval	current;
 
-	start = begin.tv_sec * 1E6 + begin.tv_usec;
+	start = begin.tv_sec * 1000 + begin.tv_usec / 1000;
 	gettimeofday(&current, NULL);
-	end = current.tv_sec * 1E6 + current.tv_usec;
-	return ((end - start) / 1E3);
+	end = current.tv_sec * 1000 + current.tv_usec / 1000;
+	return ((end - start));
 }
